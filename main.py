e82e9eb01d3175052ed76ff70e65bbd4d52c52a6
rgc292 <rgc292@nyu.edu>
2016-04-14 18:25:01 -0400

    Insert new step
    Add Clean_Data() into main() and do "from data_cleaning import Clean_Data".

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import pandas as pd
 import os
 from dataloader import loader
 from judgebioextractor import mergeWriteJudgeToClean
-
+from data_cleaning import Clean_Data
 
 def main():
     mergeWriteJudgeToClean()
+    Clean_Data()
 
 if __name__ == "__main__":
     main()
