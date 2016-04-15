a095eab1f6799df40dc0c2b5e7e0c618608f6663
rgc292 <Rafa@192.168.1.139>
2016-04-15 11:16:11 -0400

    s

--- main.py
+++ main.py
@@ -4,10 +4,20 @@ import os
 from dataloader import loader
 from judgebioextractor import mergeWriteJudgeToClean
 from data_cleaning import Clean_Data
+from bioanalyzer import analyzetext
+
 
 def main():
+<<<<<<< HEAD
     mergeWriteJudgeToClean()
     Clean_Data(feature_keepcsv, decision_scheduling_merge_final_convertedcsv, data)
+=======
+    Clean_Data(feature_keepcsv, decision_scheduling_merge_final_convertedcsv, data)
+    df = loader()
+    # bioAndgrantrate = judgeAnalyzer(df)
+    # csvwriter(bioAndgrantrate)
+    analyzetext(df)
+>>>>>>> mattyd2/master
 
 if __name__ == "__main__":
     main()
