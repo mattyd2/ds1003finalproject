a6cb491ce19df17d0328d37c4dd0cba078abfff9
Matthew Dunn <mattyd2@gmail.com>
2016-05-13 12:40:15 -0400

    moved adaboost main and adaboost pickle

--- adaboost/__main__.py
+++ adaboost/__main__.py
@@ -1,4 +1,4 @@
-from sklearn.svm import AdaBoostClassifier
+from sklearn.ensemble import AdaBoostClassifier
 import sys
 import pickle
 from atty_interactions import *
@@ -12,8 +12,10 @@ if __name__ == '__main__':
     n_estimators = [800, 400, 200, 100]
     param_grid = {'n_estimators': n_estimators, 'random_state': [4590385]}
 
+    sys.setrecursionlimit(5000)
+
     grid = test_model(model, param_grid, scale=True, model_spec_function=make_atty_interactions_df)
-    make_learning_curve_from_gridsearchcsv(grid, 'C', 'learning_curve_Ada_atty_interactions')
     output = open('Ada_atty_interactions.pkl', 'wb')
+    make_learning_curve_from_gridsearchcsv(grid, 'n_estimators', 'learning_curve_Ada_atty_interactions')
     pickle.dump(grid, output)
     output.close()
